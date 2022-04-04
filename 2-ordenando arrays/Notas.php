e19744d7dfd83630c3ab4ad5e723f3c66c7f9bfb
Tiago <tiagosantos__@live.com>
2022-04-04 08:59:11 -0400

    encontrando elementos

--- 2-ordenando arrays/Notas.php
+++ 2-ordenando arrays/Notas.php
@@ -4,9 +4,24 @@ $notas = [
     "João" => 8,
     "Roberto" => 7,
     "Maria" => 9,
-    "Vinicius" => 6,
+    "Vinicius" => null,
     "Ana" => 10,
 ];
 
 krsort($notas);
 var_dump($notas);
+
+if (is_array($notas)) {
+    echo "É um array" . PHP_EOL;
+}
+echo "Ana fez a prova" . PHP_EOL;
+var_dump(isset($notas["Vinicius"]));
+
+echo "Alguém tirou 10?" . PHP_EOL;
+var_dump(in_array(10, $notas, true));
+
+echo "Quem tirou nota 10" . PHP_EOL;
+var_dump(array_search(10, $notas, true));
+//array_key_exists = verifica se a chave existe
+// in_array = verifica se o valor existe
+// isset = verifica se a chave existe e não é nula
