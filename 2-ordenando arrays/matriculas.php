b6dcc3d8dcb4f5c55b4dd021cca901cb49872dc8
Tiago <tiagosantos__@live.com>
2022-04-04 11:11:57 -0400

    spread operator

--- 2-ordenando arrays/matriculas.php
+++ 2-ordenando arrays/matriculas.php
@@ -4,6 +4,12 @@ $matriculas2021 = ["João", "Roberto", "Maria", "Vinicius", "Ana"];
 
 $novosAlunos = ["Patrícia", "Nico", "Kilderson", "Daiane"];
 
-$alunos2022 = array_merge($matriculas2021, $novosAlunos);
+$alunos2022 = [...$matriculas2021, "Carlos Vinicius", ...$novosAlunos];
 
 var_dump($alunos2022);
+
+funcao(...[1, 2, 3]);
+
+function funcao(int $a, int $b, int $c)
+{
+}
